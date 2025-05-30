088eada4b903f0badad9bd4d8036302bc2998528
janaledencan <janaledencan@gmail.com>
2025-05-30 13:47:59 +0200

    Add AuthGuard

--- frontend/movie-app/src/app/app.routes.ts
+++ frontend/movie-app/src/app/app.routes.ts
@@ -4,12 +4,21 @@ import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { DetailsPageComponent } from './pages/details-page/details-page.component';
 import { FavouritesComponent } from './pages/favourites/favourites.component';
+import { authGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
   { path: 'user/login', component: LoginComponent },
   { path: 'user/signup', component: SignupComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'favourites', component: FavouritesComponent },
-  { path: 'movie/:id', component: DetailsPageComponent },
+  {
+    path: 'favourites',
+    component: FavouritesComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'movie/:id',
+    component: DetailsPageComponent,
+    canActivate: [authGuard],
+  },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
